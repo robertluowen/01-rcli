f36ea566382f4186f7e8740db5f71ac09fc31485
robert <757120778@qq.com>
2024-07-04 00:26:49 +0800

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, rust!");
+    let x = (1, "hello", 3.4);
+    let (a, b, c) = x;
+    println!("{} {} {}", a, b, c);
+    println!("{}", x.1);
+
+    // struct
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+
+    let origin = Point { x: 0, y: 0 };
+    println!("{} {}", origin.x, origin.y);
+
+    // tuple struct
+    struct Color(i32, i32, i32);
+    let black = Color(0, 0, 0);
+    println!("{} {} {}", black.0, black.1, black.2);
+
+    // enum
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let m = Message::Write(String::from("hello"));
 }
